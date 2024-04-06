c78b0cabb43d5c2b81380881738f00bd88de4b52
Shiggymon <Shiggy@mon>
2024-04-06 15:28:10 +0200

    Pause capture and improved Start/Stop signaling
    - implemented pause capture feature which stops capturing samples. Serial port will stay active, but samples aren't copied into display buffer.
    - improved Start / Stop feature for capturing. Its now based on signalling between the main thread and the AppMainWindow
    - implemnted start/stop/pause buttons to be inactive when nothing would be done by clicking them

--- AppMainWindow.py
+++ AppMainWindow.py
@@ -1,5 +1,6 @@
 from PyQt6 import QtCore, QtWidgets
 from PyQt6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget, QHBoxLayout, QGridLayout, QLabel, QComboBox, QDoubleSpinBox, QFrame, QGroupBox
+from PyQt6.QtCore import QTimer
 from multiprocessing import Queue, connection
 import pyqtgraph as pg
 from scipy.fft import fft,fftfreq
@@ -11,6 +12,8 @@ from typing import NamedTuple
 class MsgType(Enum):
     STARTSERIAL = auto()
     STOPSERIAL = auto()
+    SERIALSTARTED = auto()
+    SERIALSTOPPED = auto()
 
 class WindowMessage(NamedTuple):
     type: MsgType
@@ -21,7 +24,9 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__()        
         self.xs = [0]
         self.ys = [0]
-        self.useSPL = True
+        self.useSPL = True # Output fft data in dBSPL instead of Pa
+        self.captureActive = False
+        self.capturePaused = False
         self.serialPorts = list()
         self.setWindowTitle("Infraschall Analyse")
         mainWidget = QWidget()
@@ -36,10 +41,16 @@ class MainWindow(QtWidgets.QMainWindow):
         buttonReloadSerial.clicked.connect(self.reloadSerial)
         self.comboSelectSerial = QComboBox()
         self.comboSelectSerial.setEditable(True)
-        buttonStartCapture = QPushButton("Start")
-        buttonStartCapture.clicked.connect(self.startCapture)
-        buttonStopCapture = QPushButton("Stop")
-        buttonStopCapture.clicked.connect(self.stopCapture)
+        self.buttonStartCapture = QPushButton("Start")
+        self.buttonStartCapture.clicked.connect(self.startCapture)
+        self.buttonStopCapture = QPushButton("Stop")
+        self.buttonStopCapture.clicked.connect(self.stopCapture)
+        self.buttonStopCapture.setEnabled(False)
+        self.buttonPauseCapture = QPushButton("Pause")
+        self.buttonPauseCapture.clicked.connect(self.pauseCapture)
+        self.buttonPauseCapture.setCheckable(True)
+        self.buttonPauseCapture.setEnabled(False)
+
 
         # Set Top Layout with 2 Graphs
         vLayout.addLayout(hTopLayout)
@@ -61,9 +72,9 @@ class MainWindow(QtWidgets.QMainWindow):
         settingsLayout.addWidget(self.comboSelectSerial,1,1)
         settingsLayout.addWidget(buttonReloadSerial,2,1)
         settingsLayout.addWidget(QLabel(""),3,1)
-        settingsLayout.addWidget(buttonStartCapture,4,1)
-        settingsLayout.addWidget(QPushButton("Pause"),5,1)
-        settingsLayout.addWidget(buttonStopCapture,6,1)
+        settingsLayout.addWidget(self.buttonStartCapture,4,1)
+        settingsLayout.addWidget(self.buttonPauseCapture,5,1)
+        settingsLayout.addWidget(self.buttonStopCapture,6,1)
         # Settings Column 3: Display Settings
         settingsLayout.addWidget(QLabel("Data Display"),0,3)
         settingsLayout.addWidget(QDoubleSpinBox(),1,3)
@@ -86,26 +97,31 @@ class MainWindow(QtWidgets.QMainWindow):
         self.displayPipe = p
 
         # Setup Timers for periodic UI and Data Update
-        self.updDataTimer = QtCore.QTimer()
+        self.updDataTimer = QTimer()
         self.updDataTimer.setInterval(50)
         self.updDataTimer.timeout.connect(self.updateData)
         self.updDataTimer.start()
-        self.updPlotTTimer = QtCore.QTimer()
+        self.updPlotTTimer = QTimer()
         self.updPlotTTimer.setInterval(100)
         self.updPlotTTimer.timeout.connect(self.updateTPlot)
         self.updPlotTTimer.start()
-        self.updPlotFTimer = QtCore.QTimer()
+        self.updPlotFTimer = QTimer()
         self.updPlotFTimer.setInterval(200)
         self.updPlotFTimer.timeout.connect(self.updateFPlot)
         self.updPlotFTimer.start()
+        self.updCommsTimer = QTimer()
+        self.updCommsTimer.setInterval(200)
+        self.updCommsTimer.timeout.connect(self.updateComms)
+        self.updCommsTimer.start()
         
     def q2Vars(self, q, xs, ys):
         while True:
             try:
                 item = q.get(block=False)
                 for i in item:
-                    xs.append(xs[-1]+1)
-                    ys.append(i)
+                    if not self.capturePaused:
+                        xs.append(xs[-1]+1)
+                        ys.append(i)
             except:
                 break
         while(len(xs)>500):
@@ -114,10 +130,12 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def updateData(self):
          self.q2Vars(self.q, self.xs, self.ys)
+
     def updateTPlot(self):
         xs = np.array(self.xs)
         ys = np.array(self.ys)
         self.lineT.setData(xs, ys)
+
     def updateFPlot(self):
         T = 1/50 # 50 Hz
         N = len(self.ys)
@@ -128,14 +146,31 @@ class MainWindow(QtWidgets.QMainWindow):
             self.lineF.setData(xf, self.lin2dbSPL(2.0 * abs(yf[0:N//2])))
         else:
             self.lineF.setData(xf, 2.0 * abs(yf[0:N//2]))
+    
+    def updateComms(self):
+        while self.displayPipe.poll():
+            msg = self.displayPipe.recv()
+            try:
+                if msg.type == MsgType.SERIALSTARTED:
+                    self.captureActive = True
+                    self.buttonStopCapture.setEnabled(True)
+                    self.pauseCapture(pause=False)
+                elif msg.type == MsgType.SERIALSTOPPED:
+                    self.captureActive = False
+                    self.buttonStartCapture.setEnabled(True)
+                    self.buttonStopCapture.setEnabled(False)
+                    self.buttonPauseCapture.setEnabled(False)
+            except:
+                raise
+
     def lin2dbSPL(self,x):
         return 20*np.log10(x/(2*10**-5))
+    
     def reloadSerial(self):
         currentIndex = self.comboSelectSerial.currentIndex()
         currentText = self.comboSelectSerial.currentText()
         self.serialPorts = serial.tools.list_ports.comports()
         self.serialPorts.sort(key=lambda x : x.device)
-        
         # clear list
         count = self.comboSelectSerial.count()
         for i in range(0,count):
@@ -143,7 +178,6 @@ class MainWindow(QtWidgets.QMainWindow):
         # repopulate list
         for p in self.serialPorts:
             self.comboSelectSerial.addItem(p.device)
-        
         # select previously selected port. If none was selected, select first. 
         if currentIndex == -1 and currentText == "":
             self.comboSelectSerial.setCurrentIndex(0)
@@ -154,10 +188,28 @@ class MainWindow(QtWidgets.QMainWindow):
         from Infra import SerialPortSettings
         msg = WindowMessage(MsgType.STARTSERIAL, SerialPortSettings(port=self.comboSelectSerial.currentText(), baudrate=38400))
         self.displayPipe.send(msg)
+        self.buttonStartCapture.setEnabled(False)
+        QTimer.singleShot(1000, lambda : self.buttonStartCapture.setEnabled(not self.captureActive))
+        
 
     def stopCapture(self):
         msg = WindowMessage(MsgType.STOPSERIAL)
         self.displayPipe.send(msg)
+        
+
+    def pauseCapture(self, paused=None, pause=None):
+        if pause == None:
+            pause = not self.capturePaused
+        self.buttonPauseCapture.setEnabled(True)
+        if pause: 
+            self.buttonPauseCapture.setText("Continue")
+            self.buttonPauseCapture.setChecked(True)
+            self.capturePaused = True
+        else:
+            self.buttonPauseCapture.setText("Pause")
+            self.capturePaused = False
+            self.buttonPauseCapture.setChecked(False)
+        
 
 
         
